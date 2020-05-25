a66e656ad387a11f3b4e3acb8c9dcf0079287de4
vincent <vincent_fillon@hotmail.com>
2020-05-25 17:18:25 +0200

    feat:ajout fonction home qui apellera les render

---
+++ src/Controller/PrincipalController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PrincipalController extends AbstractController
+{
+    /**
+     * @Route("/principal", name="principal")
+     */
+    public function index()
+    {
+        return $this->render('principal/index.html.twig', [
+            'controller_name' => 'PrincipalController',
+        ]);
+    }
+
+    public function home(){
+        return $this->render('principal/home.html.twig');
+
+    }
+}
